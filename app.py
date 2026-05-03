fcd58bca8792cc08c509f02818a0fd1dce4f4abb
wormyboi <brownh@stu.otc.school.nz>
2026-05-04 09:10:34 +1200

    finished(?) planning, started home page

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
