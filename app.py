f2db0922553f0e7dc6fc6472002cd08c6b00362c
wormyboi <brownh@stu.otc.school.nz>
2026-05-06 11:00:38 +1200

    Started adding banner to prompt users to log in / sign up

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#Set up
+signin = "no"
+
+#App Routes
 #home page
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', signin=signin)
 
 #all modules page
 @app.route('/modules')
 def modules():
-    return render_template('modules.html')
+    return render_template('modules.html', signin=signin)
 
 #attempted modules page
 @app.route('/attempted')
 def attempted():
-    return render_template('attempted.html')
+    return render_template('attempted.html', signin=signin)
 
 #profile page
 @app.route('/profile')
