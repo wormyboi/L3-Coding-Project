ef77c1e80c16f1c9a2ed12fedeec697bda37cbaf
wormyboi <brownh@stu.otc.school.nz>
2026-05-05 12:31:27 +1200

    set up base template for website, created modules and profile html files

--- app.py
+++ app.py
@@ -7,5 +7,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+#modules page
+@app.route('/modules')
+def modules():
+    return render_template('modules.html')
+
+#profile page
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
