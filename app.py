2f59ed4aee52cac6e111efe95566998079ef7c90
wormyboi <brownh@stu.otc.school.nz>
2026-05-13 11:00:59 +1200

    Fixed code so user stays logged in when changing pages, added ability to log out

--- app.py
+++ app.py
@@ -3,28 +3,34 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 #Set up
-signin = "no"
+#If the user is signed in, username
+user = ["no", "n/a"]
 
 #App Routes
 #home page
 @app.route('/')
 def home():
-    return render_template('index.html', signin=signin)
+    return render_template('index.html', signin=user[0])
 
 #all modules page
 @app.route('/modules')
 def modules():
-    return render_template('modules.html', signin=signin)
+    return render_template('modules.html', signin=user[0])
 
 #attempted modules page
 @app.route('/attempted')
 def attempted():
-    return render_template('attempted.html', signin=signin)
+    return render_template('attempted.html', signin=user[0])
 
 #profile page
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', username=user[1], signin=user[0])
+
+#sign up page
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 #log in poage
 @app.route('/login')
@@ -43,10 +49,19 @@ def log():
         return render_template("login.html", error_statement=error_statement,
                             log_user=username, log_pass=password)
     else:
-        signin = "yes"
-        return render_template("index.html", signin=signin)
+    #Logs user in and returns them to home page
+        user[0] = "yes"
+        user[1] = username
+        return render_template("index.html", signin=user[0])
+
+#Logs user out and returns them to the home page   
+@app.route('/logout', methods=["POST"])
+def logout():
+    user[0] = "no"
+    user[1] = "n/a"
+    return render_template("index.html", signin=user[0])
 
 
-
+#Run program
 if __name__ == '__main__':
     app.run(debug=True)
