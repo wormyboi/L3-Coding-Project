a246f87d50f79523b8265dc2def8c9206f69de18
wormyboi <brownh@stu.otc.school.nz>
2026-06-09 13:31:49 +1200

    Fixed error mentioned in previous commit, made it so quiz inputs would deactivate if they had been answered, started on making submit messages on quizpage permantent and making quiz summary page work

--- app.py
+++ app.py
@@ -26,22 +26,24 @@ user_ans_rips = {}
 ans_dicts = {
     "Quiz - Rips": user_ans_rips,
 }
+ #which questions are no longer able to be answered
+q_lock = {}
 
 
 #Dictionaries containing quiz / module questions and their answers
 rips_mod = {}
 rips_quiz = {
     1: {
-        "var1": ["text", "'ans'"], "var2": ["text", "'ans'"], "var3": ["text", "'ans'"]
+        "var1": ["text", "ans"], "var2": ["text", "ans"], "var3": ["text", "ans"]
         }, 
     2: {
-        "variant1": ["radio", "'correct ans'", "'incorrect ans'", "'wrong ans'"], 
-        "varient2": ["radio", "'correct ans2'", "'not-right ans'"], 
-        "varient3": ["radio", "'correct ans3'", "'incorrect'", "'wrong'", "'very wrong'"]
+        "variant1": ["radio", "correct ans", "incorrect ans", "wrong ans"], 
+        "varient2": ["radio", "correct ans2", "not-right ans"], 
+        "varient3": ["radio", "correct ans3", "incorrect", "wrong", "very wrong"]
         },
     3: {
-        "Var 1": ["checkbox", "'ans'", "'not-ans'", "'v-not-ans'"],
-        "Var 2": ["checkbox", "'right'", "'wrong'", "'not this one'"]
+        "Var 1": ["checkbox", "ans", "not-ans", "v-not-ans"],
+        "Var 2": ["checkbox", "right", "wrong", "not this one"]
         },
     }
 waves_mod = {}
@@ -131,6 +133,7 @@ def logout():
 
 @app.route('/quiz', methods=["POST"])
 def quiz():
+    q_lock.clear()
     questions.clear()
     user_ans.clear()
     ans.clear()
@@ -172,8 +175,21 @@ def quizpage():
     num = request.form.get("q_num")
     u_ans = request.form.get("u_ans")
     no = int(num)
+    if no not in q_lock.keys():
+        q_lock[no] = False
+    if (no+1) not in q_lock.keys():
+        q_lock[no+1] = False
+    #initially setting up as though there are no messages
     msg = False
     msgtype = "none"
+    #if this is a module or a quiz
+    mod = module[0]
+    modu = mod.split()
+    if modu[0] == "Quiz":
+        tp = "quiz"
+    else:
+        tp = "module"
+    #Checking user answer or moving to the next page
     if act_type == "next":
         no +=1
         num=str(no)
@@ -181,18 +197,24 @@ def quizpage():
         if u_ans:
             q = questions[no]
             user_ans[q] = u_ans
-            if ans[q] == u_ans:
-                msg = "Correct answer! Good job!"
-                msgtype = "msg"
+            if tp == "quiz":
+                q_lock[no] = "disabled"
         else:
             msgtype = "errormsg"
             msg = "Please enter an answer"
-    mod = module[0]
-    modu = mod.split()
-    if modu[0] == "Quiz":
-        tp = "quiz"
+    if questions[no] in user_ans.keys():
+        q = questions[no]
+        q_ans = user_ans[q]
+        if q_ans == user_ans[q]:
+            msg = "Correct answer! Good job!"
+            msgtype = "msg"
+        else:
+            msg = "Incorrect"
+            msgtype = "errormsg"
     else:
-        tp = "module"
+        q_ans = False
+    #Setting var_list to the list of information for the question that's
+    #going to be displayed and setting up a list of options if necessary
     var_list = var_info[no]
     if var_list[0] != "text":
         for optn in var_list:
@@ -204,24 +226,27 @@ def quizpage():
         if len(user_ans) < len(ans):
             error_message = "Are you sure you want to finish this attempt?\nSome questions are unanswered"
             return render_template("quizbase.html", signin=user[0], quesnum=quesnum, error_message=error_message, no=no,
-                                module=module[0], questions=questions, ans=ans, tp=tp, num=num, var_list=var_list, opt=opt)
+                                module=module[0], questions=questions, ans=ans, tp=tp, num=num, var_list=var_list, opt=opt,
+                                q_lock=q_lock[no], q_ans=q_ans)
         else:
             error_message = "Are you sure you want to finish this attempt?"
             return render_template("quizbase.html", signin=user[0], quesnum=quesnum, error_message=error_message, no=no,
-                                module=module[0], questions=questions, ans=ans, tp=tp, num=num, var_list=var_list, opt=opt)
+                                module=module[0], questions=questions, ans=ans, tp=tp, num=num, var_list=var_list, opt=opt,
+                                q_lock=q_lock[no], q_ans=q_ans)
     #Sends user back to quizbase, either with the next question or their answers checked
     else:
         return render_template("quizbase.html", no=no, module=module[0], questions=questions, ans=ans, tp=tp, 
-                               quesnum=quesnum, num=num, var_list=var_list, opt=opt, signin=user[0], msg=msg, msgtype=msgtype)
+                               quesnum=quesnum, num=num, var_list=var_list, opt=opt, signin=user[0], msg=msg, msgtype=msgtype,
+                               q_lock=q_lock[no], q_ans=q_ans)
 
 @app.route('/endquiz', methods=["POST"])
 def endquiz():
     quesnum = len(questions)
     score = 0
-    #ans_dicts[module[0]] = user_ans
-    #for q in ans.keys():
-    #    if ans[q] == user_ans[q]:
-    #        score +=1
+    ans_dicts[module[0]] = user_ans
+    for q in ans.keys():
+        if ans[q] == user_ans[q]:
+            score +=1
     return render_template("endquiz.html", signin=user[0], quesnum=quesnum, score=score)
 
 
